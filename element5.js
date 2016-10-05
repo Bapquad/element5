c675aa599f79a4ee96685ad2781842aafef43c0e
unknown <vuhuycuong291987@gmail.com>
2016-10-06 00:47:14 +0700

    Fix for Find Media.

--- element5.js
+++ element5.js
@@ -72,7 +72,8 @@ function Factory()
 				module.exports.timeline5 = __webpack_require__( 3 ); 
 				module.exports.media5 = __webpack_require__( 4 );
 				module.exports.request5 = __webpack_require__( 5 ); 
-				module.exports.solution5 = __webpack_require__( 6 );
+				module.exports.bom5 = __webpack_require__( 6 );
+				module.exports.solution5 = __webpack_require__( 7 ); 
 			},
 			function( module ) 								// pack require ( 1 ) 
 			{
@@ -126,9 +127,9 @@ function Factory()
 				
 				var effect = function( el, css, self ) 
 				{
-					if( el.Css == undefined && css != 0 ) 
+					if( el.CssComm == undefined && css != 0 ) 
 					{
-						el.Css = css;
+						el.CssComm = css;
 					} 
 
 					// Case of body
@@ -194,7 +195,7 @@ function Factory()
 								}
 								else 						// Multiple
 								{
-									var childs = []; WZQB8524uscm
+									var childs = [];
 									for( var i = 0; i < len; i++ ) 
 									{
 										var el = collect[ i ]; 
@@ -478,8 +479,8 @@ function Factory()
 					{
 						if( this.CssSelf )
 							this.CssSelf.SetStyles( styles ); 
-						else if( this.Css )
-							this.Css.SetStyles( styles ); 
+						else if( this.CssComm )
+							this.CssComm.SetStyles( styles ); 
 						return this;
 					}, 
 					
@@ -487,30 +488,68 @@ function Factory()
 					{
 						if( this.CssSelf ) 
 							this.CssSelf.SetStyle( name, value ); 
-						else if( this.Css )
-							this.Css.SetStyle( name, value ); 
+						else if( this.CssComm )
+							this.CssComm.SetStyle( name, value ); 
 						else 
 							this.style[ name ] = value;
 						return this;
 					}, 
 					
-					SetStyle: function( name, value ) 
+					SetStyle: function( name, value, com ) 
 					{
+						if( com ) 
+						{
+							if( this.CssComm )
+								this.CssComm.SetStyle( name, value ); 
+							else if( this.CssSelf ) 
+								this.CssSelf.SetStyle( name, value ); 
+							else 
+								this.style[ name ] = value; 
+							return this;
+						}
 						if( this.CssSelf ) 
 							this.CssSelf.SetStyle( name, value ); 
-						else if( this.Css )
-							this.Css.SetStyle( name, value ); 
+						else if( this.CssComm )
+							this.CssComm.SetStyle( name, value ); 
 						else 
 							this.style[ name ] = value;
 						return this;
 					}, 
 					
-					SetStyles: function( styles ) 
+					css: function() 
 					{
+						var el = this;
+						if( arguments.length == 2 )
+						{
+							return el.SetStyle( arguments[0], arguments[1] );;
+						}
+						return el.SetStyles( arguments[0] );
+					},
+					
+					Css: function() 
+					{
+						var el = this;
+						if( arguments.length == 2 )
+						{
+							return el.SetStyle( arguments[0], arguments[1], 'com' );;
+						}
+						return el.SetStyles( arguments[0], 'com' );
+					},  
+					
+					SetStyles: function( styles, com ) 
+					{
+						if( com === 'com' ) 
+						{
+							if( this.CssComm )
+								this.CssComm.SetStyles( styles ); 
+							else if( this.CssSelf )
+								this.CssSelf.SetStyles( styles ); 
+							return this;
+						}
 						if( this.CssSelf )
 							this.CssSelf.SetStyles( styles ); 
-						else if( this.Css )
-							this.Css.SetStyles( styles ); 
+						else if( this.CssComm )
+							this.CssComm.SetStyles( styles ); 
 						return this;
 					}, 
 					
@@ -692,9 +731,9 @@ function Factory()
 						{
 							selector = this.CssSelf.selectorText;
 						} 
-						else if( this.Css != undefined ) 
+						else if( this.CssComm != undefined ) 
 						{
-							selector = this.Css.selectorText;
+							selector = this.CssComm.selectorText;
 						} 
 						
 						return media.product.Find( selector ); 
@@ -739,7 +778,7 @@ function Factory()
 						{
 							var clsnAttr = this.getAttribute( 'class' );
 							var classies = clsnAttr.trim().split( ' ' ); 
-							classies.find( function( value, index, arr ) 
+							classies.forEach( function( value, index ) 
 							{
 								if( value == clsn ) 
 								{
@@ -891,24 +930,22 @@ function Factory()
 								return el.AddData( name, value );
 							}
 							
-							var searchResult = el.BackData.find( function( item, index, currValue ) 
+							var len = el.BackData.length;
+							
+							for( var i = 0; i < len; i++ ) 
 							{
+								var item = el.BackData[ i ];
 								if( item != undefined ) 
 								{
 									if( item.indexOf( name ) === 2 ) 
 									{
 										el.BackData[ index ] = ( '{"' + name + '":' + el.JSonString( value ) + '}' ); 
-										return true;
+										return el;
 									}
 								}
-							}); 
-						
-							if( searchResult == undefined ) 
-							{
-								return el.AddData( name, value );
-							} 
+							}
 							
-							return el;
+							return el.AddData( name, value );
 						} 
 						catch( err ) 
 						{
@@ -929,27 +966,15 @@ function Factory()
 						} 
 						else 
 						{
-							var pos;
-							var searchResult = el.BackData.find( function( item, index, currValue ) 
+							var len = el.BackData.length;
+							for( var i = 0; i < len; i++ ) 
 							{
-								if( item != undefined ) 
+								if( el.BackData[ i ].indexOf( name ) === 2 ) 
 								{
-									if( item.indexOf( name ) === 2 ) 
-									{
-										pos = index;
-										return true;
-									}
+									return el.ParseJSON( el.BackData[ i ] )[ name ]; 
 								}
-							}); 
-							
-							if( pos >= 0 ) 
-							{
-								return el.ParseJSON( el.BackData[ pos ] )[ name ]; 
 							}
-							else 
-							{
-								return -1;
-							}
+							return -1;
 						}
 					}, 
 					
@@ -959,19 +984,15 @@ function Factory()
 						{
 							if( name == undefined ) throw "Delete the data need the name specify.";
 							var el = this;
-							var result;
+							var len = el.BackData.length;
 							
-							var searchResult = el.BackData.find( function( item, index, currValue ) 
+							for( var i = 0; i < len; i++ ) 
 							{
-								if( item != undefined ) 
+								if( el.BackData[ i ].indexOf( name ) === 2 ) 
 								{
-									if( item.indexOf( name ) === 2 ) 
-									{
-										result = delete el.BackData[ index ];
-										return true;
-									} 
-								}
-							}); 
+									return delete el.BackData[ index ]; 
+								} 
+							}
 						} 
 						catch( err ) 
 						{
@@ -1317,6 +1338,38 @@ function Factory()
 					return ( deep + time );
 				};
 				
+				element5.CreateBlock = function( block ) 
+				{
+					var len = block.length;
+					if( !len ) 
+						return;
+					
+					var down = '>';
+					var up = '^';
+					var root = element5.Create( 'div' );
+					var curr = root;
+					block = block.join( ' ' ).split( ' ' );
+					len = block.length;
+					
+					for( var i = 0; i < len; i++ ) 
+					{
+						var codes = block[ i ];
+						var posId = codes . indexOf ( '@' ); 
+						var id = codes.substr( posId + 1 );
+						var codes = codes.substr( 0, posId );
+						var pat = /([\.|\#|\[a-z]{1,})+/; 
+						console.log( codes );
+						console.log( pat.exec(codes) );
+						curr[ id ] = { id : id }; 
+						console.log( curr );
+						curr = curr[ id ];	// Move to next point.
+						console.log( curr );
+						break;
+					} 
+					
+					return root;
+				};
+				
 				element5.Create = function( tagName ) 
 				{
 					var pat = ( /^[a-zA-Z0-9]+$/ ) . exec ( tagName );
@@ -1415,13 +1468,15 @@ function Factory()
 				Style.AddLine = function( selector ) 
 				{
 					var index = Style.sheet.cssRules.length; 
-					
 					Style.sheet.attach( selector + ' {}', index ); 
-					
 					var rule = Style.sheet.cssRules[ index ];
-					
 					return element5.Extend( rule, Style.ruleModifier ); 
 				}; 
+				
+				Style.AddCss = function( selector ) 
+				{
+					return Style.AddLine( selector );
+				};
 				
 				Style.AddTimeLine = function( timeline ) 
 				{
@@ -1571,6 +1626,10 @@ function Factory()
 						media.attach( selector + ' {}', index ); 
 						return element5.Extend( media.cssRules[ index ], Style.ruleModifier );
 					}, 
+					AddCss: function( selector ) 
+					{
+						return this.AddLine( selector );
+					}, 
 					GetDescription: function() 
 					{
 						return this.media[ 0 ];
@@ -1579,11 +1638,41 @@ function Factory()
 				
 				Style.ruleModifier = 
 				{
+					AddSelector: function( selector ) 
+					{
+						var rule = this;
+						rule.selectorText = rule.selectorText + ', ' + selector.trim(); 
+						return rule;
+					},
 					SetStyle: function( name, value ) 
 					{
 						this.style[name] = value;
 						return this;
 					}, 
+					
+					css: function() 
+					{
+						if( arguments.length ) 
+						{
+							if( arguments.length == 1 )
+								this.SetStyles( arguments[0] );
+							else if( arguments.length == 2 ) 
+								this.SetStyle( arguments[0], arguments[1] );
+						}
+						return this;
+					}, 
+					
+					Css: function() 
+					{
+						if( arguments.length ) 
+						{
+							if( arguments.length == 1 )
+								this.SetStyles( arguments[0] );
+							else if( arguments.length == 2 ) 
+								this.SetStyle( arguments[0], arguments[1] );
+						}
+						return this;
+					},
 					
 					AddStyle: function( name, value ) 
 					{
@@ -1618,12 +1707,51 @@ function Factory()
 				
 				module.exports = Timeline;
 			}, 
-			function( module, __webpack_require__ ) 		// pack require ( 5 )
+			function( module, __webpack_require__ ) 		// pack require ( 4 )
 			{
 				var Media = 
 				{
-					
-				}
+					CreateVideo: function() 
+					{
+						// var video = element5.Create( 'video' );
+						// video.css( {'background': 'red'} );
+						// video.id = 'videoTag'; 
+
+						// navigator.mediaDevices.getUserMedia( { video: { facingMode: "user" } } )
+						// .then( function ( stream ) 
+						// {
+							// var video = document.getElementById('videoTag');
+							// video.srcObject = stream;
+						// }).catch( function ( error ) 
+						// {
+							// console.log(error.name + ": " + error.message);
+						// });
+					}, 
+					CreateAudio: function() 
+					{
+						var audioContext = new AudioContext();
+
+						navigator.mediaDevices.getUserMedia({
+
+							audio: true
+
+						}).then(function (stream) {
+
+							var sourceNode = audioContext.createMediaStreamSource(stream);
+
+							var gainNode = audioContext.createGain();
+
+							sourceNode.connect(gainNode);
+
+						}).catch( function (error) {
+
+							console.log(error.name + ": " + error.message);
+
+						});
+					}
+				};
+				
+				module.exports = Media;
 			},
 			function( module, __webpack_require__ )  		// pack require ( 5 ) 
 			{
@@ -1642,6 +1770,111 @@ function Factory()
 				module.exports = Request;
 			}, 
 			function( module, __webpack_require__ ) 		// pack require ( 6 ) 
+			{
+				var Bom = 			// Browser Object Model.
+				{
+					Browser: 
+					{
+						// Opera 8.0+
+						IsOpera: (function() 
+						{
+							return ((!!window.opr && !!opr.addons) || !!window.opera || navigator.userAgent.indexOf(' OPR/') >= 0);
+						})(),
+						
+						// Firefox 1.0+ 
+						IsFirefox: (function() 
+						{
+							return (typeof InstallTrigger !== 'undefined'); 
+						})(), 
+						
+						// At least Safari 3+
+						IsSafari: (function()
+						{
+							return (Object.prototype.toString.call(window.HTMLElement).indexOf('Constructor') > 0);
+						})(), 
+						
+						// Internet Explorer 6-11
+						IsIE: (function()
+						{
+							return (/*@cc_on!@*/false || !!document.documentMode);
+						})(),
+						
+						// Edge 20+
+						IsEdge: (function()
+						{
+							return (!(/*@cc_on!@*/false || !!document.documentMode) && !!window.StyleMedia);
+						})(), 
+						
+						// Chrome 1+
+						IsChrome: (function()
+						{
+							return (!!window.chrome && !!window.chrome.webstore);
+						})(), 
+						
+						// Blink engine detection
+						IsBlink: (function()
+						{
+							return (((!!window.chrome && !!window.chrome.webstore) || ((!!window.opr && !!opr.addons) || !!window.opera || navigator.userAgent.indexOf(' OPR/') >= 0)) && !!window.CSS);
+						})(), 
+						
+						IsOperaMini: (function()
+						{
+							return (navigator.userAgent.match(/Opera Mini/i));
+						})(),
+					}, 
+					
+					Device: 
+					{
+						IsAndroid: (function()
+						{
+							return (/android/i.test(navigator.userAgent.toLowerCase()));
+						})(), 
+						
+						IsBlackBerry: (function()
+						{
+							return (/blackberry/i.test(navigator.userAgent.toLowerCase()));
+						})(),
+						
+						IsiOs: (function()
+						{
+							return (/ipad|iphone|ipod/i.test(navigator.userAgent.toLowerCase())); 
+						})(), 
+						
+						IsiPhone: (function()
+						{
+							return (/iphone/i.test(navigator.userAgent.toLowerCase()));
+						})(), 
+						
+						IsiPad: (function()
+						{
+							return (/ipad/i.test(navigator.userAgent.toLowerCase()));
+						})(), 
+						
+						IsiPos: (function()
+						{
+							return (/ipod/i.test(navigator.userAgent.toLowerCase()));
+						})(), 
+						
+						IsWP: (function()
+						{
+							return (/windows phone/i.test(navigator.userAgent.toLowerCase()));
+						})(), 
+						
+						IsWebOS: (function()
+						{
+							return (/webos/i.test(navigator.userAgent.toLowerCase()));
+						})(), 
+						
+						IsMobile: (function()
+						{
+							return (/(android|bb\d+|meego).+mobile|avantgo|bada\/|blackberry|blazer|compal|elaine|fennec|hiptop|iemobile|ip(hone|od)|ipad|iris|kindle|Android|Silk|lge |maemo|midp|mmp|netfront|opera m(ob|in)i|palm( os)?|phone|p(ixi|re)\/|plucker|pocket|psp|series(4|6)0|symbian|treo|up\.(browser|link)|vodafone|wap|windows (ce|phone)|xda|xiino/i.test(navigator.userAgent) || /1207|6310|6590|3gso|4thp|50[1-6]i|770s|802s|a wa|abac|ac(er|oo|s\-)|ai(ko|rn)|al(av|ca|co)|amoi|an(ex|ny|yw)|aptu|ar(ch|go)|as(te|us)|attw|au(di|\-m|r |s )|avan|be(ck|ll|nq)|bi(lb|rd)|bl(ac|az)|br(e|v)w|bumb|bw\-(n|u)|c55\/|capi|ccwa|cdm\-|cell|chtm|cldc|cmd\-|co(mp|nd)|craw|da(it|ll|ng)|dbte|dc\-s|devi|dica|dmob|do(c|p)o|ds(12|\-d)|el(49|ai)|em(l2|ul)|er(ic|k0)|esl8|ez([4-7]0|os|wa|ze)|fetc|fly(\-|_)|g1 u|g560|gene|gf\-5|g\-mo|go(\.w|od)|gr(ad|un)|haie|hcit|hd\-(m|p|t)|hei\-|hi(pt|ta)|hp( i|ip)|hs\-c|ht(c(\-| |_|a|g|p|s|t)|tp)|hu(aw|tc)|i\-(20|go|ma)|i230|iac( |\-|\/)|ibro|idea|ig01|ikom|im1k|inno|ipaq|iris|ja(t|v)a|jbro|jemu|jigs|kddi|keji|kgt( |\/)|klon|kpt |kwc\-|kyo(c|k)|le(no|xi)|lg( g|\/(k|l|u)|50|54|\-[a-w])|libw|lynx|m1\-w|m3ga|m50\/|ma(te|ui|xo)|mc(01|21|ca)|m\-cr|me(rc|ri)|mi(o8|oa|ts)|mmef|mo(01|02|bi|de|do|t(\-| |o|v)|zz)|mt(50|p1|v )|mwbp|mywa|n10[0-2]|n20[2-3]|n30(0|2)|n50(0|2|5)|n7(0(0|1)|10)|ne((c|m)\-|on|tf|wf|wg|wt)|nok(6|i)|nzph|o2im|op(ti|wv)|oran|owg1|p800|pan(a|d|t)|pdxg|pg(13|\-([1-8]|c))|phil|pire|pl(ay|uc)|pn\-2|po(ck|rt|se)|prox|psio|pt\-g|qa\-a|qc(07|12|21|32|60|\-[2-7]|i\-)|qtek|r380|r600|raks|rim9|ro(ve|zo)|s55\/|sa(ge|ma|mm|ms|ny|va)|sc(01|h\-|oo|p\-)|sdk\/|se(c(\-|0|1)|47|mc|nd|ri)|sgh\-|shar|sie(\-|m)|sk\-0|sl(45|id)|sm(al|ar|b3|it|t5)|so(ft|ny)|sp(01|h\-|v\-|v )|sy(01|mb)|t2(18|50)|t6(00|10|18)|ta(gt|lk)|tcl\-|tdg\-|tel(i|m)|tim\-|t\-mo|to(pl|sh)|ts(70|m\-|m3|m5)|tx\-9|up(\.b|g1|si)|utst|v400|v750|veri|vi(rg|te)|vk(40|5[0-3]|\-v)|vm40|voda|vulc|vx(52|53|60|61|70|80|81|83|85|98)|w3c(\-| )|webc|whit|wi(g |nc|nw)|wmlb|wonu|x700|yas\-|your|zeto|zte\-/i.test(navigator.userAgent.substr(0,4)));
+						})(), 
+					}
+				};
+				
+				module.exports = Bom;
+			}, 
+			function( module, __webpack_require__ ) 		// pack require ( 7 ) 
 			{
 				var Solution = {};
 				
